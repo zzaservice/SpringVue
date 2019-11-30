3ddb908cd4af3ac808c42807db1b807bbf383d42
zzaservice <982908910@qq.com>
2019-11-30 11:31:13 +0800

    对启动类改变了

--- src/main/java/com/qf/SpringUserApplication.java
+++ src/main/java/com/qf/SpringUserApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringUserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringUserApplication.class);
+        System.out.println("启动类改变了");
+        SpringApplication .run(SpringUserApplication.class);
     }
 }
